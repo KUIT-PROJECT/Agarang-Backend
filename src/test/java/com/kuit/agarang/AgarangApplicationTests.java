334a437bb6a4608b72902cfb4ea06920a1c8d06a
JGoo99 <zzgg3040@naver.com>
2024-07-01 17:08:29 +0900

    first commit

---
+++ src/test/java/com/kuit/agarang/AgarangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuit.agarang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgarangApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
