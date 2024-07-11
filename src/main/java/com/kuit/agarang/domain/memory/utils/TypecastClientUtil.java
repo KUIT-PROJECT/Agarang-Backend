0f919d3b28bda5a27faedbc9925f46be3a90a251
JGoo99 <zzgg3040@naver.com>
2024-07-11 19:35:18 +0900

    refactor: typecastClientUtil -> web, type 로 클래스 분리

--- src/main/java/com/kuit/agarang/domain/memory/utils/TypecastClientUtil.java
+++ src/main/java/com/kuit/agarang/domain/memory/utils/TypecastClientUtil.java
@@ -1,42 +1,25 @@
 package com.kuit.agarang.domain.memory.utils;
 
-import com.kuit.agarang.domain.memory.config.WebClientConfig;
 import com.kuit.agarang.domain.memory.dto.TypecastRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class TypecastClientUtil {
 
-  private final WebClientConfig webClientConfig;
+  private final WebClientUtil webClientUtil;
   @Value("${typecast.baseUrl}")
   private String baseUrl;
   @Value("${typecast.apiKey}")
   private String apiKey;
 
   public <T> T post(String uri, TypecastRequest requestDto, Class<T> responseClass) {
-    return webClientConfig.webClient().post()
-      .uri(baseUrl + uri)
-      .header("Authorization", "Bearer " + apiKey)
-      .bodyValue(requestDto)
-      .retrieve()
-      // TODO : Exception 처리 추가 후 수정하기
-      .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> clientResponse.bodyToMono(String.class).map(Exception::new))
-      .bodyToMono(responseClass)
-      .block();
+    return webClientUtil.post(baseUrl + uri, apiKey, requestDto, responseClass);
   }
 
-  public <T> T get(String uri, Class<T> responseClass) {
-    return webClientConfig.webClient().get()
-      .uri(uri)
-      .header("Authorization", "Bearer " + apiKey)
-      .retrieve()
-      // TODO : Exception 처리 추가 후 수정하기
-      .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> clientResponse.bodyToMono(String.class).map(Exception::new))
-      .bodyToMono(responseClass)
-      .block();
+  public <T> T get(String url, Class<T> responseClass) {
+    return webClientUtil.get(url, apiKey, responseClass);
   }
 }
