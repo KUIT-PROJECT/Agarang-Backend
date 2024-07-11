337fcbace5066cbb23a3a4a0ec985ecf68a72075
JGoo99 <zzgg3040@naver.com>
2024-07-11 19:19:11 +0900

    refactor: tts 컨트롤러 삭제 & dto 어노테이션 정리

--- src/main/java/com/kuit/agarang/domain/memory/dto/MessageRequest.java
+++ src/main/java/com/kuit/agarang/domain/memory/dto/MessageRequest.java
@@ -1,13 +1,10 @@
 package com.kuit.agarang.domain.memory.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class MessageRequest {
-  // TODO : 텍스트 외의 추가적으로 받을 값이 있는지 확인하기
   private String text;
 }
--- src/main/java/com/kuit/agarang/domain/memory/dto/TypecastResponse.java
+++ src/main/java/com/kuit/agarang/domain/memory/dto/TypecastResponse.java
@@ -1,20 +1,15 @@
 package com.kuit.agarang.domain.memory.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class TypecastResponse {
   private Result result;
 
   @Getter
-  @ToString
-  @AllArgsConstructor
   @NoArgsConstructor
   public static class Result {
     @JsonProperty("speak_v2_url")
