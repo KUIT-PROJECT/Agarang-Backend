87a2d81dec0a9a9d07799f8d23c1c2e1baeaf98c
JGoo99 <zzgg3040@naver.com>
2024-07-11 19:19:11 +0900

    refactor: tts 컨트롤러 삭제 & dto 어노테이션 정리

--- src/main/java/com/kuit/agarang/domain/memory/dto/TypecastErrorResponse.java
+++ src/main/java/com/kuit/agarang/domain/memory/dto/TypecastErrorResponse.java
@@ -1,17 +1,14 @@
 package com.kuit.agarang.domain.memory.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class TypecastErrorResponse {
   private Message message;
 
   @Getter
-  @AllArgsConstructor
   @NoArgsConstructor
   public static class Message {
     private String msg;
--- src/main/java/com/kuit/agarang/domain/memory/dto/TypecastRequest.java
+++ src/main/java/com/kuit/agarang/domain/memory/dto/TypecastRequest.java
@@ -1,12 +1,12 @@
 package com.kuit.agarang.domain.memory.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class TypecastRequest {
 
   @JsonProperty("actor_id")
@@ -24,6 +24,20 @@ public class TypecastRequest {
   private String modelVersion;
   @JsonProperty("xapi_audio_format")
   private String xapiAudioFormat;
+
+  @Builder
+  public TypecastRequest(String actorId, String text, String lang, Double tempo, Integer volume, Integer pitch, Boolean xapiHd, Integer maxSeconds, String modelVersion, String xapiAudioFormat) {
+    this.actorId = actorId;
+    this.text = text;
+    this.lang = lang;
+    this.tempo = tempo;
+    this.volume = volume;
+    this.pitch = pitch;
+    this.xapiHd = xapiHd;
+    this.maxSeconds = maxSeconds;
+    this.modelVersion = modelVersion;
+    this.xapiAudioFormat = xapiAudioFormat;
+  }
 
   public static TypecastRequest create(MessageRequest request) {
     return TypecastRequest.builder()
