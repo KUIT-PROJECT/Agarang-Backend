a3f00ffe441ef1542873260511f4d2f484076826
JGoo99 <zzgg3040@naver.com>
2024-07-12 15:44:31 +0900

    docs: enum 파일 domain 하위로 이동

--- src/main/java/com/kuit/agarang/domain/memory/enums/Genre.java
+++ src/main/java/com/kuit/agarang/domain/memory/enums/Genre.java
@@ -1,4 +1,4 @@
-package com.kuit.agarang.domain.memory.model.enums;
+package com.kuit.agarang.domain.memory.enums;
 
 public enum Genre {
   BALLAD, POP, JAZZ, ACOUSTIC, RNB, ELECTRONIC, ROCK, INDIE, HIPHOP
