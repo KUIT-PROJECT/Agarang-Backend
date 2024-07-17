b9177b35bc016c7a926e827977343277915fd841
Han Ju Kim <hjkim2902@gmail.com>
2024-07-17 17:41:44 +0900

    feat : BaseResponse & BaseResponseStatus 추가

---
+++ src/main/java/com/kuit/agarang/global/common/model/dto/BaseResponseStatus.java
@@ -0,0 +1,29 @@
+package com.kuit.agarang.global.common.model.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum BaseResponseStatus {
+  SUCCESS(true, HttpStatus.OK, 200, "요청에 성공하였습니다."),
+  BAD_REQUEST(false, HttpStatus.BAD_REQUEST, 400, "입력값을 확인해주세요."),
+  UNAUTHORIZED(false, HttpStatus.UNAUTHORIZED, 401, "인증이 필요합니다."),
+  FORBIDDEN(false, HttpStatus.FORBIDDEN, 403, "권한이 없습니다."),
+  NOT_FOUND(false, HttpStatus.NOT_FOUND, 404, "대상을 찾을 수 없습니다.");
+
+
+  private final boolean isSuccess;
+  @JsonIgnore
+  private final HttpStatus httpStatus;
+  private final int code;
+  private final String message;
+
+  BaseResponseStatus(boolean isSuccess, HttpStatus httpStatus, int code, String message) {
+    this.isSuccess = isSuccess;
+    this.httpStatus = httpStatus;
+    this.code = code;
+    this.message = message;
+  }
+}
