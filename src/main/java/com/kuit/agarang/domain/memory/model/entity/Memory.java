9c73477fff94939ecd39a9073fd4522de2eb0ad1
Bae <bjy556@gmail.com>
2024-07-24 01:59:05 +0900

    fix : musicUrl 추가

--- src/main/java/com/kuit/agarang/domain/memory/model/entity/Memory.java
+++ src/main/java/com/kuit/agarang/domain/memory/model/entity/Memory.java
@@ -31,6 +31,7 @@ public class Memory extends BaseEntity {
   private Baby baby;
 
   private String imageUrl;
+  private String musicUrl;
   private String text;
 
   @Enumerated(EnumType.STRING)
@@ -49,9 +50,10 @@ public class Memory extends BaseEntity {
   private List<Hashtag> hashtag;
 
   @Builder
-  public Memory(String imageUrl, String text, Genre genre,
+  public Memory(String imageUrl, String musicUrl, String text, Genre genre,
                 Mood mood, Tempo tempo, Instrument instrument) {
     this.imageUrl = imageUrl;
+    this.musicUrl = musicUrl;
     this.text = text;
     this.genre = genre;
     this.mood = mood;
