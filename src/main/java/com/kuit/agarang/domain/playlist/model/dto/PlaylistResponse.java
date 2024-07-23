e2b47709cbd0d09598f075b225f882a321b8d9f3
Bae <bjy556@gmail.com>
2024-07-24 02:06:32 +0900

    feat : PlaylistResponse class 추가

---
+++ src/main/java/com/kuit/agarang/domain/playlist/model/dto/PlaylistResponse.java
@@ -0,0 +1,12 @@
+package com.kuit.agarang.domain.playlist.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaylistResponse {
+    private PlaylistDto playlist;
+}
