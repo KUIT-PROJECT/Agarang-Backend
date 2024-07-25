a00c8a6c7a0508137a938c26e0b8c6251aa1c632
JinGoo <zzgg3040@naver.com>
2024-07-25 22:08:31 +0900

    [Feature] TTS 요청 시 webhook 으로 응답 받기 (#47)
    * refactor: post 타입 제너릭 변경
    
    * feat: tts 요청 webhook 구현

--- src/main/java/com/kuit/agarang/domain/ai/model/dto/MessageRequest.java
+++ src/main/java/com/kuit/agarang/domain/ai/model/dto/MessageRequest.java
@@ -1,10 +1,13 @@
 package com.kuit.agarang.domain.ai.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class MessageRequest {
+  // TODO : actor 정보 추가 (남아 or 여아)
   private String text;
 }
