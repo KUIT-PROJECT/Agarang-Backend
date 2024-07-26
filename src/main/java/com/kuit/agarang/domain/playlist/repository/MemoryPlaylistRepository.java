31f47656dbe0a566d655ff411a7ee5b6ca106c58
Bae <bjy556@gmail.com>
2024-07-27 03:14:37 +0900

    refactor : 불필요한 @Repository  제거

--- src/main/java/com/kuit/agarang/domain/playlist/repository/MemoryPlaylistRepository.java
+++ src/main/java/com/kuit/agarang/domain/playlist/repository/MemoryPlaylistRepository.java
@@ -2,11 +2,9 @@ package com.kuit.agarang.domain.playlist.repository;
 
 import com.kuit.agarang.domain.playlist.model.entity.MemoryPlaylist;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface MemoryPlaylistRepository extends JpaRepository<MemoryPlaylist, Long> {
     List<MemoryPlaylist> findByPlaylistId(Long playlistId);
 }
