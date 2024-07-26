52322ac59e6d59b1ad57074884597cdaf7b33a92
Bae <bjy556@gmail.com>
2024-07-27 03:39:24 +0900

    feat : 메서드 이름 변경 및 DB 조회 로직 변경

--- src/main/java/com/kuit/agarang/domain/playlist/service/PlaylistService.java
+++ src/main/java/com/kuit/agarang/domain/playlist/service/PlaylistService.java
@@ -1,18 +1,15 @@
 package com.kuit.agarang.domain.playlist.service;
 
-import com.kuit.agarang.domain.memory.model.dto.MemoryDTO;
 import com.kuit.agarang.domain.memory.model.entity.Memory;
+import com.kuit.agarang.domain.playlist.model.entity.MusicBookmark;
 import com.kuit.agarang.domain.memory.repository.MemoryRepository;
-import com.kuit.agarang.domain.playlist.model.dto.MusicUrlsResponse;
-import com.kuit.agarang.domain.playlist.model.dto.PlaylistDto;
-import com.kuit.agarang.domain.playlist.model.dto.PlaylistResponse;
-import com.kuit.agarang.domain.playlist.model.dto.PlaylistsResponse;
+import com.kuit.agarang.domain.playlist.model.dto.*;
 import com.kuit.agarang.domain.playlist.model.entity.MemoryPlaylist;
 import com.kuit.agarang.domain.playlist.model.entity.Playlist;
 import com.kuit.agarang.domain.playlist.repository.MemoryPlaylistRepository;
 import com.kuit.agarang.domain.playlist.repository.PlaylistRepository;
+import com.kuit.agarang.domain.playlist.repository.MusicBookmarkRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,41 +22,43 @@ public class PlaylistService {
     private final PlaylistRepository playlistRepository;
     private final MemoryPlaylistRepository memoryPlaylistRepository;
     private final MemoryRepository memoryRepository;
+    private final MusicBookmarkRepository musicBookmarkRepository;
 
     public PlaylistsResponse getAllPlaylists() {
         List<Playlist> playlists = playlistRepository.findAll();
         List<PlaylistDto> playlistDtos = playlists.stream()
-                .map(playlist -> new PlaylistDto(playlist.getId(), playlist.getName(), playlist.getImageUrl()))
+                .map(PlaylistDto::of)
                 .collect(Collectors.toList());
 
         return new PlaylistsResponse(playlistDtos);
     }
 
-    public PlaylistResponse getPlaylistById(Long id) {
-        // TODO : 예외처리 바꾸기 (AgarangException)
-        Playlist playlist = playlistRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Playlist not found"));
+    public PlaylistTracksResponse getPlaylistTracks(Long playlistId, Long memberId) {
+        // TODO : 인가처리에 따라 수정
+        // TODO : 예외처리 수정
+        Playlist playlist = playlistRepository.findById(playlistId)
+                .orElseThrow(() -> new RuntimeException("playlistId : "+ playlistId + " 해당 플레이리스트가 존재하지 않습니다."));
 
-        PlaylistDto playlistDto = new PlaylistDto(playlist.getId(), playlist.getName(), playlist.getImageUrl());
+        PlaylistDto playlistDto = PlaylistDto.of(playlist);
 
-        return new PlaylistResponse(playlistDto);
-    }
-
-    public MusicUrlsResponse getMusicUrlsByPlaylistId(Long playlistId, Long memberId) {
-        // TODO : 예외처리 필요
         List<MemoryPlaylist> memoryPlaylists = memoryPlaylistRepository.findByPlaylistId(playlistId);
 
-        List<Long> memoryIds = memoryPlaylists.stream()
-                .map(memoryPlaylist -> memoryPlaylist.getMemory().getId())
+        List<MusicDto> musicDtos = memoryPlaylists.stream()
+                .map(memoryPlaylist -> {
+                    Memory memory = memoryRepository.findById(memoryPlaylist.getMemory().getId())
+                            .orElseThrow(() -> new RuntimeException("member 접근 오류"));
+
+                    boolean isBookmarked = checkBookmarkStatus(memory, memberId);
+
+                    return MusicDto.of(memory, isBookmarked);
+                })
                 .collect(Collectors.toList());
 
-        List<Memory> memories = memoryRepository.findAllById(memoryIds);
+        return new PlaylistTracksResponse(playlistDto, musicDtos);
+    }
 
-        List<String> musicUrls= memories.stream()
-                .filter(memory -> memory.getMember().getId().equals(memberId))
-                .map(Memory::getMusicUrl)
-                .collect(Collectors.toList());
-
-        return new MusicUrlsResponse(musicUrls);
+    private boolean checkBookmarkStatus(Memory memory, Long memberId) {
+        MusicBookmark musicBookmark = musicBookmarkRepository.findByMemoryAndMemberId(memory, memberId);
+        return musicBookmark != null;
     }
 }
