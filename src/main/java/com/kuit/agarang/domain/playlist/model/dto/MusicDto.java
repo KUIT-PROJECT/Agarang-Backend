8a897312c4b471bf1e9806ff142faf34200d7d20
Bae <bjy556@gmail.com>
2024-07-27 03:16:42 +0900

    feat : MusicDto 추가

---
+++ src/main/java/com/kuit/agarang/domain/playlist/model/dto/MusicDto.java
@@ -0,0 +1,35 @@
+package com.kuit.agarang.domain.playlist.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kuit.agarang.domain.memory.model.entity.Memory;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+public class MusicDto {
+    private String imageUrl;
+    private String musicTitle;
+    private String musicUrl;
+    private String mood;
+    private boolean isBookmarked;
+
+    @Builder
+    public MusicDto(String imageUrl, String musicTitle, String musicUrl, String mood, boolean isBookmarked) {
+        this.imageUrl = imageUrl;
+        this.musicTitle = musicTitle;
+        this.musicUrl = musicUrl;
+        // TODO : mood -> hashtag 여러개로 수정
+        this.mood = mood;
+        this.isBookmarked = isBookmarked;
+    }
+
+    public static MusicDto of(Memory memory, boolean isBookmarked) {
+        return MusicDto.builder()
+                .imageUrl(memory.getImageUrl())
+                .musicTitle(memory.getMusicTitle())
+                .musicUrl(memory.getMusicUrl())
+                .mood(memory.getMood().toString())
+                .isBookmarked(isBookmarked)
+                .build();
+    }
+}
