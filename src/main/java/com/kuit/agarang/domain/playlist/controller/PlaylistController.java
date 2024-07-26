151ab6c8ce7a41eec8cb9568e6dce95561a8b8c2
Bae <bjy556@gmail.com>
2024-07-27 03:39:24 +0900

    feat : 메서드 이름 변경 및 DB 조회 로직 변경

--- src/main/java/com/kuit/agarang/domain/playlist/controller/PlaylistController.java
+++ src/main/java/com/kuit/agarang/domain/playlist/controller/PlaylistController.java
@@ -1,27 +1,18 @@
 package com.kuit.agarang.domain.playlist.controller;
 
-import com.kuit.agarang.domain.playlist.model.dto.MusicUrlsResponse;
-import com.kuit.agarang.domain.playlist.model.dto.PlaylistDto;
-import com.kuit.agarang.domain.playlist.model.dto.PlaylistResponse;
+import com.kuit.agarang.domain.playlist.model.dto.PlaylistTracksResponse;
 import com.kuit.agarang.domain.playlist.model.dto.PlaylistsResponse;
 import com.kuit.agarang.domain.playlist.service.PlaylistService;
 import com.kuit.agarang.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/playlists")
 @RequiredArgsConstructor
 public class PlaylistController {
 
-    /*
-     *   TODO : ResponseEntity 사용
-     *   TODO :
-     */
     private final PlaylistService playlistService;
 
     @GetMapping
@@ -30,15 +21,10 @@ public class PlaylistController {
         return ResponseEntity.ok(new BaseResponse<>(playlistsResponse));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<BaseResponse> getPlaylistById(@PathVariable Long id) {
-        PlaylistResponse playlistResponse = playlistService.getPlaylistById(id);
-        return ResponseEntity.ok(new BaseResponse<>(playlistResponse));
-    }
-
-    @GetMapping("/{id}/music")
-    public ResponseEntity<BaseResponse> getMusicUrlsByPlaylistId(@PathVariable Long id, @RequestParam Long memberId) {
-        MusicUrlsResponse musicUrlsResponse = playlistService.getMusicUrlsByPlaylistId(id, memberId);
-        return ResponseEntity.ok(new BaseResponse<>(musicUrlsResponse));
+    @GetMapping("/{id}/track")
+    public ResponseEntity<BaseResponse<PlaylistTracksResponse>> getPlaylistTracks(@PathVariable Long id,
+                                                                                  @RequestParam Long memberId) {
+        PlaylistTracksResponse playlistTracksResponse = playlistService.getPlaylistTracks(id, memberId);
+        return ResponseEntity.ok(new BaseResponse<>(playlistTracksResponse));
     }
 }
