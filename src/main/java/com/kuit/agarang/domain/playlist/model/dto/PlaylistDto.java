cc388a06fc69f1e304a47e7b05ad87edb6aa6b38
Bae <bjy556@gmail.com>
2024-07-27 03:20:24 +0900

    Refactor : PlaylistDto 클래스 빌더 패턴 추가

--- src/main/java/com/kuit/agarang/domain/playlist/model/dto/PlaylistDto.java
+++ src/main/java/com/kuit/agarang/domain/playlist/model/dto/PlaylistDto.java
@@ -1,20 +1,30 @@
 package com.kuit.agarang.domain.playlist.model.dto;
 
+import com.kuit.agarang.domain.playlist.model.entity.Playlist;
 import lombok.*;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@NoArgsConstructor
 public class PlaylistDto {
+
     private Long id;
     private String name;
     private String imageUrl;
 
+    @Builder
     public PlaylistDto(Long id, String name, String imageUrl) {
         this.id = id;
         this.name = name;
         this.imageUrl = imageUrl;
+    }
+
+    public static PlaylistDto of(Playlist playlist) {
+        return PlaylistDto.builder()
+                .id(playlist.getId())
+                .name(playlist.getName())
+                .imageUrl(playlist.getImageUrl())
+                .build();
     }
 }
