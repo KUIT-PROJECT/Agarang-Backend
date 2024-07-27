c2d6bf30f95523d92c44ef667fe7ef2123d58554
AppleMint98 <hong721816@konkuk.ac.kr>
2024-07-27 09:42:00 +0900

    fix : branch rebase

--- src/main/java/com/kuit/agarang/global/common/config/SecurityConfig.java
+++ src/main/java/com/kuit/agarang/global/common/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -26,10 +25,20 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
     http
-      .cors((cors) -> cors.disable()) // 모든 도메인 일시적 허용
-      .csrf((auth) -> auth.disable()) // csrf 일시적 비활성화
-      .formLogin((auth) -> auth.disable()) // 기본 로그인 폼 비활성화
-      .httpBasic((auth) -> auth.disable()); // HTTP 헤더 인증 방식 비활성화
+        .cors((cors) -> cors.disable()) // 모든 도메인 일시적 허용
+        .csrf((auth) -> auth.disable()) // csrf 일시적 비활성화
+        .formLogin((auth) -> auth.disable()) // 기본 로그인 폼 비활성화
+        .httpBasic((auth) -> auth.disable()); // HTTP 헤더 인증 방식 비활성화
+
+    http
+        .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+    //oauth2
+    http
+        .oauth2Login((oauth2) -> oauth2
+            .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+                .userService(customOAuth2UserService)))
+            .successHandler(customSuccessHandler));
 
     http
         .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
@@ -44,8 +53,8 @@ public class SecurityConfig {
     http
         .authorizeHttpRequests((auth) -> auth
             .requestMatchers(
-              "/env", "/api-json/**", "/api-docs", "/swagger-ui/**",
-              "/oauth2/**")
+                "/env", "/api-json/**", "/api-docs", "/swagger-ui/**",
+                "/oauth2/**")
             .permitAll()
             .requestMatchers("/reissue").permitAll()
             .anyRequest().permitAll() // TODO : 인가 구현 후 수정
