b784ccd2e95b60d90501d12c65798ebf9bcfe8e7
JinGoo <zzgg3040@naver.com>
2024-07-30 16:57:37 +0900

    [Feature] Redis 캐시 구현 및 ai-service 반영 (#60)
    * fix: deploy 용 컨트롤러 common 파일로 이동 & 불필요 dto 삭제
    
    * fix: deploy 관련 불필요 변수 삭제
    
    * feat: redis 환경 구축 & crud test
    
    * fix: 임시 jpa entity -> redis dto 변경
    
    * refactor: (image->질문1) historyMessage 캐시 적용 & 테스트
    
    * refactor: (image->질문1) typecast 캐시 적용 & 테스트
    
    * fix: redis 필요 없는 필드 삭제
    
    * refactor: redis GET null 예외 처리 (optional)

--- src/main/java/com/kuit/agarang/domain/ai/repository/GPTChatHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kuit.agarang.domain.ai.repository;
-
-import com.kuit.agarang.domain.ai.model.entity.GPTChatHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GPTChatHistoryRepository extends JpaRepository<GPTChatHistory, String> {
-}
---
+++ src/test/java/com/kuit/agarang/global/common/service/RedisServiceTest.java
@@ -0,0 +1,132 @@
+package com.kuit.agarang.global.common.service;
+
+import com.kuit.agarang.domain.ai.model.dto.gpt.*;
+import com.kuit.agarang.domain.ai.model.entity.cache.GPTChatHistory;
+import com.kuit.agarang.domain.ai.model.enums.GPTRole;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RedisServiceTest {
+
+  @Autowired
+  private RedisService redisService;
+  private static final String KEY = "agarang";
+
+  @AfterEach
+  void delete() {
+    redisService.delete(KEY);
+  }
+
+  @Test
+  void saveStringAndGet() {
+    // given
+    String value = "test-value";
+    // when
+    redisService.save(KEY, value);
+    // then
+    assertEquals(value, redisService.get(KEY, String.class).orElseThrow(() -> new RuntimeException("")));
+  }
+
+  @Test
+  void updateString() {
+    // given
+    String value = "test-value";
+    redisService.save(KEY, value);
+    // when
+    String newValue = "test2-value";
+    redisService.update(KEY, newValue);
+    // then
+    assertEquals(newValue, redisService.get(KEY, String.class).orElseThrow(() -> new RuntimeException("")));
+  }
+
+  @Test
+  void saveStringWithExpiry() throws InterruptedException {
+    // given
+    String value = "test-value";
+    redisService.saveWithExpiry(KEY, value, 1000L, TimeUnit.MILLISECONDS);
+    // when
+    Thread.sleep(1000);
+    // then
+    assertEquals(Optional.empty(), redisService.get(KEY, String.class));
+  }
+
+  @Test
+  void deleteString() {
+    // given
+    String value = "test-value";
+    redisService.save(KEY, value);
+    // when
+    redisService.delete(KEY);
+    // then
+    assertEquals(Optional.empty(), redisService.get(KEY, String.class));
+  }
+
+  @Test
+  void existsByKey_true() {
+    // given
+    String value = "test-value";
+    // when
+    redisService.save(KEY, value);
+    // then
+    assertTrue(redisService.existsByKey(KEY));
+  }
+
+  @Test
+  void existsByKey_false() {
+    // given
+    String value = "test-value";
+    // when
+    redisService.save(KEY, value);
+    redisService.delete(KEY);
+    // then
+    assertFalse(redisService.existsByKey(KEY));
+  }
+
+  @Test
+  void saveGPTChatAndGet() {
+    // given
+    // gpt 질문
+    GPTMessage requestMessage = GPTMessage.builder()
+      .role(GPTRole.USER)
+      .content("오늘은 무슨 요일이야?")
+      .build();
+    GPTRequest request = new GPTRequest(new ArrayList<>(List.of(requestMessage)));
+
+    // gpt 대답
+    GPTMessage responseMessage = GPTMessage.builder()
+      .role(GPTRole.ASSISTANT)
+      .content("오늘은 화요일이야.")
+      .build();
+    GPTImageDescription imageDescription =
+      new GPTImageDescription("맑고 푸른 하늘 아래 투명한 바다와 부드러운 모래사장이 어우러진 평화로운 해변 풍경입니다.", List.of("바다", "여름", "모래성"));
+
+    request.getMessages().add(responseMessage); // 질문, 대답 합친 history message 포함해서 선언
+    GPTChatHistory chatHistory = GPTChatHistory.builder()
+      .imageTempPath("images/image.jpeg")
+      .hashtags(imageDescription.getNoun())
+      .historyMessages(request.getMessages())
+      .build();
+
+    // when
+    redisService.save(KEY, chatHistory);
+
+    // then
+    GPTChatHistory savedChatHistory = redisService.get(KEY, GPTChatHistory.class)
+      .orElseThrow(() -> new RuntimeException(""));
+    assertEquals(chatHistory.getImageTempPath(), savedChatHistory.getImageTempPath());
+    assertEquals(chatHistory.getHashtags(), savedChatHistory.getHashtags());
+    assertEquals(
+      "[{\"role\":\"user\",\"content\":\"오늘은 무슨 요일이야?\"},{\"role\":\"assistant\",\"content\":\"오늘은 화요일이야.\"}]",
+      savedChatHistory.getHistoryMessage());
+  }
+}
