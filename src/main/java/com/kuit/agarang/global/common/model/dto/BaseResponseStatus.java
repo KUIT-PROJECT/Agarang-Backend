8c02dcfe2293a276a99dce00c023e778ac6665a5
Han Ju Kim <56250226+rlagkswn00@users.noreply.github.com>
2024-07-30 21:09:38 +0900

    Feat : 추억 다이어리 즐겨찾기, 수정, 삭제 로직 추가 (#58)
    * feat : BaeResponse 빈생성자 추가, 에러상태값 추가
    
    * feat : Memory 연관 레포지토리 생성
    
    * feat : 수정, 삭제, 즐겨찾기 요청 DTO 추가
    
    * feat : 생성자 추가
    
    * feat : 업데이트 도메인 로직 추가
    
    * feat : 수정, 삭제, 즐겨찾기 로직 구현
    
    * fix : BookmarkRequest 필드 이름 수정
    
    * fix : Long타입 변경으로 인한 파싱 제거
    
    * fix : DTO 필드 명 변경

--- src/main/java/com/kuit/agarang/global/common/model/dto/BaseResponseStatus.java
+++ src/main/java/com/kuit/agarang/global/common/model/dto/BaseResponseStatus.java
@@ -11,7 +11,11 @@ public enum BaseResponseStatus {
   BAD_REQUEST(false, HttpStatus.BAD_REQUEST, 400, "입력값을 확인해주세요."),
   UNAUTHORIZED(false, HttpStatus.UNAUTHORIZED, 401, "인증이 필요합니다."),
   FORBIDDEN(false, HttpStatus.FORBIDDEN, 403, "권한이 없습니다."),
-  NOT_FOUND(false, HttpStatus.NOT_FOUND, 404, "대상을 찾을 수 없습니다.");
+  NOT_FOUND(false, HttpStatus.NOT_FOUND, 404, "대상을 찾을 수 없습니다."),
+
+  INVALID_MEMORY_ID(false,HttpStatus.NOT_FOUND, 4001, "존재하지 않는 추억입니다."),
+
+  ;
 
 
   private final boolean isSuccess;
