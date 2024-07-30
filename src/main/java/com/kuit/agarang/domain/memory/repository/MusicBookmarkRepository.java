52ac23f482d4032fe8e053955495de8af0b84ffc
Han Ju Kim <hjkim2902@gmail.com>
2024-07-30 21:24:02 +0900

    fix : 중복된 빈 제거

--- src/main/java/com/kuit/agarang/domain/memory/repository/MusicBookmarkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kuit.agarang.domain.memory.repository;
-
-import com.kuit.agarang.domain.memory.model.entity.Memory;
-import com.kuit.agarang.domain.memory.model.entity.MusicBookmark;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicBookmarkRepository extends JpaRepository<MusicBookmark, Long> {
-  void deleteByMemory(Memory memory);
-}
