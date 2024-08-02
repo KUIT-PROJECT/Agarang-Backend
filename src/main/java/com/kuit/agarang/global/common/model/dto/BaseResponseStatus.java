8623662dd1a58eb15bb12014a59936501bda33fe
Jin Goo <126454114+JGoo99@users.noreply.github.com>
2024-08-02 18:59:41 +0900

    [Hot-Fix] 삭제된 BaseresRonse 임포트 삭제 & 에러처리 전략 설계 (#71)
    * fix: 삭제된 baseresponse 임포트 제거
    
    * fix: 비지니스 로직 exception 네이밍 변경
    
    * feat: 전역 exception 처리
    
    * feat: 파일 크기 초과 exception 처리
    
    * refactor: 디버깅용 stack trace 프린트
    
    * feat: redis 연결 실패 및 전역 에러 처리 구현
    
    * feat: 컨테이너 네트워크 연결 명령어 추가

--- src/main/java/com/kuit/agarang/global/common/model/dto/BaseResponseStatus.java
+++ src/main/java/com/kuit/agarang/global/common/model/dto/BaseResponseStatus.java
@@ -13,7 +13,11 @@ public enum BaseResponseStatus {
   FORBIDDEN(false, HttpStatus.FORBIDDEN, 403, "권한이 없습니다."),
   NOT_FOUND(false, HttpStatus.NOT_FOUND, 404, "대상을 찾을 수 없습니다."),
 
-  INVALID_MEMORY_ID(false,HttpStatus.NOT_FOUND, 4001, "존재하지 않는 추억입니다."),
+  INVALID_MEMORY_ID(false, HttpStatus.NOT_FOUND, 4001, "존재하지 않는 추억입니다."),
+  FILE_TOO_LARGE(false, HttpStatus.PAYLOAD_TOO_LARGE, 4002, "최대 파일 사이즈(1MB)를 초과했습니다."),
+
+  SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR, 5001, "알 수 없는 이유로 서버에 문제가 발생했습니다."),
+  FAIL_REDIS_CONNECTION(false, HttpStatus.SERVICE_UNAVAILABLE, 5002, "레디스 서버에 연결 실패했습니다.")
 
   ;
 
