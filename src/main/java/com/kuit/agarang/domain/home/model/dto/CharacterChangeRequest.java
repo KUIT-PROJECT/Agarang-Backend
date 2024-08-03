8d84658cab07b17d311e35e4d2d464108f1e5326
AppleMint98 <hong721816@konkuk.ac.kr>
2024-08-03 18:08:48 +0900

    fix : 기본 생성자 추가

--- src/main/java/com/kuit/agarang/domain/home/model/dto/CharacterChangeRequest.java
+++ src/main/java/com/kuit/agarang/domain/home/model/dto/CharacterChangeRequest.java
@@ -2,8 +2,10 @@ package com.kuit.agarang.domain.home.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CharacterChangeRequest {
   private Long characterId;
