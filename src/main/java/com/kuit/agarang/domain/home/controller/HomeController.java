727175b11983f5fdf413aef33f19dd061718475c
AppleMint98 <hong721816@konkuk.ac.kr>
2024-08-03 17:19:43 +0900

    fix : 컨트롤러 URL 수정

--- src/main/java/com/kuit/agarang/domain/home/controller/HomeController.java
+++ src/main/java/com/kuit/agarang/domain/home/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/app/home")
+@RequestMapping("/api/home")
 @RequiredArgsConstructor
 public class HomeController {
 
--- src/main/java/com/kuit/agarang/domain/home/controller/SettingController.java
+++ src/main/java/com/kuit/agarang/domain/home/controller/SettingController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/app/home/setting")
+@RequestMapping("/api/home/setting")
 @RequiredArgsConstructor
 public class SettingController {
 
