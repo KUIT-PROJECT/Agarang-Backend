0c6000fcd7a44d4f0c03d9317bc281a3367eb635
JGoo99 <zzgg3040@naver.com>
2024-08-06 13:33:05 +0900

    feat: gpt 응답 json 파싱

--- src/main/java/com/kuit/agarang/domain/ai/model/dto/gpt/GPTImageDescription.java
+++ src/main/java/com/kuit/agarang/domain/ai/model/dto/gpt/GPTImageDescription.java
@@ -1,9 +1,5 @@
 package com.kuit.agarang.domain.ai.model.dto.gpt;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kuit.agarang.global.common.exception.exception.OpenAPIException;
-import com.kuit.agarang.global.common.model.dto.BaseResponseStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,14 +14,4 @@ import java.util.List;
 public class GPTImageDescription {
   private String text;
   private List<String> noun;
-
-  public static GPTImageDescription from(String content) {
-    ObjectMapper objectMapper = new ObjectMapper();
-    try {
-      return objectMapper.readValue(content, GPTImageDescription.class);
-    } catch (JsonProcessingException e) {
-      log.info("gpt's image description : {}", content);
-      throw new OpenAPIException(BaseResponseStatus.INVALID_GPT_RESPONSE);
-    }
-  }
 }
