3dc9c6ce9749aa92cb5e7d9104dbdc2547af1a6e
AppleMint98 <hong721816@konkuk.ac.kr>
2024-08-07 10:59:37 +0900

    refactor : 아기 등록 과정 통합

--- src/main/java/com/kuit/agarang/domain/member/service/MemberService.java
+++ src/main/java/com/kuit/agarang/domain/member/service/MemberService.java
@@ -26,11 +26,8 @@ public class MemberService {
 
   private final MemberRepository memberRepository;
   private final BabyRepository babyRepository;
-  private final AuthenticationUtil authenticationUtil;
 
   public void verifyBabyCode(String providerId, String babyCode) {
-
-    log.info("providerId = {}", providerId);
 
     Baby baby = babyRepository.findByBabyCode(babyCode)
         .orElseThrow(() -> new BusinessException(NOT_FOUND_BABY));
@@ -41,35 +38,28 @@ public class MemberService {
     member.addBaby(baby);
   }
 
-  public void assignFamilyRole(String familyRole) {
-
-    String providerId = authenticationUtil.getProviderId();
-    log.info("providerId = {}", providerId);
-
-    memberRepository.updateFamilyRoleByProviderId(providerId, familyRole);
-  }
-
-  public void saveNewBaby(String providerId, String babyName, LocalDate dueDate) {
+  public void processBabyAssignment(String providerId, String babyName, LocalDate dueDate, String familyRole) {
 
     Member member = memberRepository.findByProviderId(providerId)
         .orElseThrow(() -> new BusinessException(NOT_FOUND_MEMBER));
 
-    String babyCode;
-    do {
-      babyCode = CodeUtil.generateUniqueCode();
-    } while (babyRepository.existsByBabyCode(babyCode));
+    if (member.getBaby() == null) {
 
-    if (babyRepository.existsByBabyCode(babyCode)) {
-      throw new BusinessException(BaseResponseStatus.DUPLICATE_BABY_CODE);
+      String babyCode;
+      do {
+        babyCode = CodeUtil.generateUniqueCode();
+      } while (babyRepository.existsByBabyCode(babyCode));
+
+      Baby baby = Baby.builder()
+          .name(babyName)
+          .expectedDueAt(dueDate)
+          .babyCode(babyCode)
+          .build();
+
+      babyRepository.save(baby);
+      member.addBaby(baby);
     }
 
-    Baby baby = Baby.builder()
-        .name(babyName)
-        .expectedDueAt(dueDate)
-        .babyCode(babyCode)
-        .build();
-
-    member.addBaby(baby);
-    babyRepository.save(baby);
+    memberRepository.updateFamilyRoleByProviderId(providerId, familyRole);
   }
 }
