0427e45babb807bdfc94ef347e0b6aeefa830eae
AppleMint98 <hong721816@konkuk.ac.kr>
2024-08-07 17:07:57 +0900

    refactor : memberRepository 메서드 삭제 fix : getMemberId 오타수정 "Id" -> "memberId"

--- src/main/java/com/kuit/agarang/domain/member/repository/MemberRepository.java
+++ src/main/java/com/kuit/agarang/domain/member/repository/MemberRepository.java
@@ -2,17 +2,10 @@ package com.kuit.agarang.domain.member.repository;
 
 import com.kuit.agarang.domain.member.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
   Optional<Member> findByProviderId(String providerId);
-
-  @Modifying
-  @Query("UPDATE Member m SET m.familyRole = :familyRole WHERE m.id = :id")
-  void updateFamilyRoleById(Long id, String familyRole);
 }
