0e1741fd101bf1d6fa0ede033d4a33165dd29c92
JGoo99 <zzgg3040@naver.com>
2024-08-17 08:00:52 +0900

    back to 630c9c0

--- src/main/java/com/kuit/agarang/domain/login/controller/ReissueController.java
+++ src/main/java/com/kuit/agarang/domain/login/controller/ReissueController.java
@@ -6,6 +6,7 @@ import com.kuit.agarang.domain.login.utils.CookieUtil;
 import com.kuit.agarang.global.common.model.dto.BaseResponse;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.kuit.agarang.global.common.model.dto.BaseResponseStatus.SUCCESS;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ReissueController {
@@ -25,8 +27,10 @@ public class ReissueController {
                                                           HttpServletResponse response) {
     ReissueDto reissueDto = jwtService.reissueTokens(refresh);
 
-    response.addCookie(cookieUtil.createCookie("ACCESS", reissueDto.getNewAccessToken()));
+    response.addHeader("Authorization", reissueDto.getNewAccessToken());
     response.addCookie(cookieUtil.createCookie("REFRESH", reissueDto.getNewRefreshToken()));
+    log.info("New Access Token = {}", reissueDto.getNewAccessToken());
+    log.info("New Refresh Token = {}", reissueDto.getNewRefreshToken());
 
     return ResponseEntity.ok(new BaseResponse<>(SUCCESS));
   }
