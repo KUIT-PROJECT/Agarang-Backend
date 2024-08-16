630c9c08a3847f80febc7ca0b7769eca8f4c1470
JGoo99 <zzgg3040@naver.com>
2024-08-17 07:21:58 +0900

    feat: refresh token redis 저장

--- src/main/java/com/kuit/agarang/domain/login/service/JWTService.java
+++ src/main/java/com/kuit/agarang/domain/login/service/JWTService.java
@@ -3,10 +3,9 @@ package com.kuit.agarang.domain.login.service;
 import com.kuit.agarang.domain.login.model.dto.ReissueDto;
 import com.kuit.agarang.domain.login.utils.JWTUtil;
 import com.kuit.agarang.domain.member.model.entity.Member;
-import com.kuit.agarang.domain.member.model.entity.RefreshToken;
 import com.kuit.agarang.domain.member.repository.MemberRepository;
-import com.kuit.agarang.domain.member.repository.RefreshRepository;
 import com.kuit.agarang.global.common.exception.exception.BusinessException;
+import com.kuit.agarang.global.common.service.RedisService;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,61 +14,55 @@ import org.springframework.transaction.annotation.Transactional;
 import static com.kuit.agarang.global.common.model.dto.BaseResponseStatus.*;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
+@RequiredArgsConstructor
 public class JWTService {
 
   private final JWTUtil jwtUtil;
+  private final RedisService redisService;
   private final MemberRepository memberRepository;
-  private final RefreshRepository refreshRepository;
 
-  public ReissueDto reissueTokens(String refresh) {
+  public ReissueDto reissueTokens(String oldRefresh) {
 
     // RefreshToken 유효성 검증
-    Member member = validateRefreshToken(refresh);
+    Member member = validateRefreshToken(oldRefresh);
 
     // AccessToken 생성 및 Refresh Rotate
     String newAccess = jwtUtil.createAccessToken(member.getProviderId(), member.getRole(), member.getId());
     String newRefresh = jwtUtil.createRefreshToken(member.getProviderId(), member.getRole(), member.getId());
 
     // Refresh Token Update
-    member.getRefreshToken().setValue(newRefresh);
+    redisService.delete(oldRefresh);
+    redisService.save(newRefresh, member.getId());
 
     return ReissueDto.builder()
-        .newAccessToken(newAccess)
-        .newRefreshToken(newRefresh)
-        .providerId(member.getProviderId())
-        .role(member.getRole())
-        .build();
+      .newAccessToken(newAccess)
+      .newRefreshToken(newRefresh)
+      .providerId(member.getProviderId())
+      .role(member.getRole())
+      .build();
   }
 
-  private Member validateRefreshToken(String refresh) {
-    // not found or null
-    RefreshToken refreshToken = refreshRepository.findByValue(refresh)
+  private Member validateRefreshToken(String oldRefresh) {
+    if (oldRefresh == null) {
+      throw new BusinessException(NOT_FOUND_REFRESH_TOKEN);
+    }
+
+    Long memberId = redisService.get(oldRefresh, Long.class)
       .orElseThrow(() -> new BusinessException(NOT_FOUND_REFRESH_TOKEN));
 
-    Member member = memberRepository.findByRefreshToken(refreshToken)
-      .orElseThrow(() -> {
-        deleteRefreshToken(refreshToken); // 탈취 가능성
-        return new BusinessException(NOT_FOUND_REFRESH_TOKEN);
-      });
-
     try {
-      jwtUtil.isExpired(refresh);
+      jwtUtil.isExpired(oldRefresh);
     } catch (ExpiredJwtException e) {
       throw new BusinessException(EXPIRED_REFRESH_TOKEN);
     }
 
-    String category = jwtUtil.getCategory(refresh);
+    String category = jwtUtil.getCategory(oldRefresh);
     if (!category.equals("refresh")) {
       throw new BusinessException(INVALID_REFRESH_TOKEN);
     }
 
-    return member;
-  }
-
-  private void deleteRefreshToken(RefreshToken refreshToken) {
-    refreshRepository.delete(refreshToken);
-    memberRepository.deleteByRefreshToken(refreshToken);
+    return memberRepository.findById(memberId)
+      .orElseThrow(() -> new BusinessException(NOT_FOUND_MEMBER));
   }
 }
--- src/main/java/com/kuit/agarang/domain/member/repository/MemberRepository.java
+++ src/main/java/com/kuit/agarang/domain/member/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.kuit.agarang.domain.member.repository;
 
 import com.kuit.agarang.domain.member.model.entity.Member;
-import com.kuit.agarang.domain.member.model.entity.RefreshToken;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,11 +16,4 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
   @Query("SELECT m FROM Member m JOIN FETCH Baby b ON m.baby = b WHERE m.id = :memberId")
   Optional<Member> findByIdFetchJoinBaby(@Param("memberId") Long memberId);
-
-  @Query("SELECT m FROM Member m JOIN FETCH m.refreshToken WHERE m.id = :memberId")
-  Optional<Member> findByIdWithRefreshToken(@Param("memberId") Long memberId);
-
-  void deleteByRefreshToken(RefreshToken refreshToken);
-
-  Optional<Member> findByRefreshToken(RefreshToken refreshToken);
 }
