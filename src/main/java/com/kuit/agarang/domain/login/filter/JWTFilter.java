b2194a7152b09e8a64af8930b8f5defa1e2086ad
JGoo99 <zzgg3040@naver.com>
2024-08-17 08:34:10 +0900

    feat: JWTException 커스텀

--- src/main/java/com/kuit/agarang/domain/login/filter/JWTFilter.java
+++ src/main/java/com/kuit/agarang/domain/login/filter/JWTFilter.java
@@ -3,7 +3,7 @@ package com.kuit.agarang.domain.login.filter;
 import com.kuit.agarang.domain.login.model.dto.CustomOAuth2User;
 import com.kuit.agarang.domain.login.utils.JWTUtil;
 import com.kuit.agarang.domain.member.model.dto.MemberDTO;
-import com.kuit.agarang.global.common.exception.exception.BusinessException;
+import com.kuit.agarang.global.common.exception.exception.JWTException;
 import com.kuit.agarang.global.common.model.dto.BaseResponseStatus;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
@@ -18,7 +18,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -38,7 +37,7 @@ public class JWTFilter extends OncePerRequestFilter {
     }
 
     if (!accessToken.startsWith(BEARER)) {
-      throw new BusinessException(BaseResponseStatus.INVALID_ACCESS_TOKEN);
+      throw new JWTException(BaseResponseStatus.INVALID_ACCESS_TOKEN);
     }
     accessToken = accessToken.split("Bearer ")[1];
 
@@ -48,14 +47,14 @@ public class JWTFilter extends OncePerRequestFilter {
     try {
       jwtUtil.isExpired(accessToken);
     } catch (ExpiredJwtException e) {
-      throw new BusinessException(BaseResponseStatus.EXPIRED_ACCESS_TOKEN);
+      throw new JWTException(BaseResponseStatus.EXPIRED_ACCESS_TOKEN);
     }
 
     // 토큰이 access인지 확인 (발급시 페이로드에 명시)
     String category = jwtUtil.getCategory(accessToken);
 
     if (!category.equals("Authorization")) {
-      throw new BusinessException(BaseResponseStatus.INVALID_ACCESS_TOKEN);
+      throw new JWTException(BaseResponseStatus.INVALID_ACCESS_TOKEN);
     }
 
     String providerId = jwtUtil.getProviderId(accessToken);
