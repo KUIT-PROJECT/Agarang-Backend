eb34aac91af17aa91d569396ce8115316a685e28
Jin Goo <zzgg3040@naver.com>
2024-08-19 05:08:52 +0900

    fix: delete-error-log

--- src/main/java/com/kuit/agarang/global/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kuit/agarang/global/common/exception/handler/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package com.kuit.agarang.global.common.exception.handler;
 import com.kuit.agarang.global.common.exception.exception.BusinessException;
 import com.kuit.agarang.global.common.model.dto.BaseResponse;
 import com.kuit.agarang.global.common.model.dto.BaseResponseStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,12 +13,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
 
-  private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
   @ExceptionHandler(BusinessException.class)
   public ResponseEntity<BaseResponse<BaseResponseStatus>> handleBusinessException(BusinessException e) {
-    LOG.error("BusinessException occurred: {}", e.getMessage());
     return ResponseEntity.status(e.getBaseResponseStatus().getHttpStatus())
-        .body(new BaseResponse<>(e.getBaseResponseStatus()));
+      .body(new BaseResponse<>(e.getBaseResponseStatus()));
   }
 }
