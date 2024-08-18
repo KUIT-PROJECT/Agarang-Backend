d09d217f772476f211016d18a7b9dee978f8b393
Jin Goo <zzgg3040@naver.com>
2024-08-19 06:38:21 +0900

    feat: 캐릭터 레벨 2 이미지 구현 (#105)

--- src/main/java/com/kuit/agarang/domain/home/service/HomeService.java
+++ src/main/java/com/kuit/agarang/domain/home/service/HomeService.java
@@ -2,12 +2,11 @@ package com.kuit.agarang.domain.home.service;
 
 import com.kuit.agarang.domain.ai.service.AIService;
 import com.kuit.agarang.domain.baby.model.entity.Baby;
-import com.kuit.agarang.domain.baby.model.entity.Character;
 import com.kuit.agarang.domain.baby.repository.BabyRepository;
 import com.kuit.agarang.domain.home.model.dto.HomeResponse;
 import com.kuit.agarang.domain.member.model.entity.Member;
 import com.kuit.agarang.domain.member.repository.MemberRepository;
-import com.kuit.agarang.domain.memory.model.entity.Memory;
+import com.kuit.agarang.domain.memory.model.dto.MemoryImageDTO;
 import com.kuit.agarang.domain.memory.repository.MemoryRepository;
 import com.kuit.agarang.global.common.exception.exception.BusinessException;
 import com.kuit.agarang.global.common.model.dto.BaseResponseStatus;
@@ -19,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -31,37 +29,32 @@ public class HomeService {
   private final MemberRepository memberRepository;
   private final MemoryRepository memoryRepository;
   private final AIService aiService;
+  private final CharacterService characterService;
 
   public HomeResponse getHome(Long memberId) {
 
-    LocalDate today = LocalDate.now();
-    log.info("today = {}", today);
-
     Baby baby = babyRepository.findByMemberId(memberId)
         .orElseThrow(() -> new BusinessException(BaseResponseStatus.NOT_FOUND_BABY));
-    String babyName = baby.getName();
 
+    LocalDate today = LocalDate.now();
     LocalDate dueDate = baby.getDueDate();
     Integer dDay = (int) ChronoUnit.DAYS.between(today, dueDate);
-    log.info("dDay = {}", dDay);
 
-    Character character = baby.getCharacter();
-    String characterUrl = character.getImageUrl();
+    String characterUrl = characterService.getCharacterImage(baby);
 
     Member member = memberRepository.findById(memberId)
         .orElseThrow(() -> new BusinessException(BaseResponseStatus.NOT_FOUND_MEMBER));
-    String speechBubble = aiService.getCharacterBubble(character, member.getFamilyRole());
+    String speechBubble = aiService.getCharacterBubble(baby.getCharacter(), member.getFamilyRole());
 
-    List<Memory> recentImages = memoryRepository.findTop3ByBabyOrderByCreatedAtDesc(baby);
-    List<Long> recentImageIds = recentImages.stream()
-        .map(Memory::getId).collect(Collectors.toList());
+    List<MemoryImageDTO> resentMemories =
+      memoryRepository.findTop3ByBabyOrderByCreatedAtDesc(baby).stream().map(MemoryImageDTO::from).toList();
 
     return HomeResponse.builder()
         .today(today)
-        .babyName(babyName)
+        .babyName(baby.getName())
         .dDay(dDay)
         .characterUrl(characterUrl)
         .speechBubble(speechBubble)
-        .memoryIds(recentImageIds).build();
+        .memories(resentMemories).build();
   }
 }
