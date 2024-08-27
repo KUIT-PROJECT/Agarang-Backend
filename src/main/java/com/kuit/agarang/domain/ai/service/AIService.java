a6b140964c6fcce360292de9ede622ee5dd31259
JinGoo <zzgg3040@naver.com>
2024-08-27 15:20:07 +0900

    fix: tts get방식 & historyMessage 저장 방식

--- src/main/java/com/kuit/agarang/domain/ai/service/AIService.java
+++ src/main/java/com/kuit/agarang/domain/ai/service/AIService.java
@@ -1,7 +1,12 @@
 package com.kuit.agarang.domain.ai.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kuit.agarang.domain.ai.model.dto.*;
+import com.kuit.agarang.domain.ai.model.dto.MemoryTextInfo;
+import com.kuit.agarang.domain.ai.model.dto.MusicAnswer;
+import com.kuit.agarang.domain.ai.model.dto.MusicInfo;
+import com.kuit.agarang.domain.ai.model.dto.QuestionResponse;
+import com.kuit.agarang.domain.ai.model.dto.QuestionResult;
+import com.kuit.agarang.domain.ai.model.dto.TextAnswer;
 import com.kuit.agarang.domain.ai.model.dto.gpt.GPTChat;
 import com.kuit.agarang.domain.ai.model.dto.gpt.GPTImageDescription;
 import com.kuit.agarang.domain.ai.model.dto.gpt.GPTMessage;
@@ -14,12 +19,10 @@ import com.kuit.agarang.domain.member.model.entity.Member;
 import com.kuit.agarang.domain.member.repository.MemberRepository;
 import com.kuit.agarang.domain.memory.model.entity.Hashtag;
 import com.kuit.agarang.domain.memory.model.entity.Memory;
-import com.kuit.agarang.domain.memory.repository.HashTagRepository;
 import com.kuit.agarang.domain.memory.repository.MemoryRepository;
 import com.kuit.agarang.domain.playlist.model.entity.MemoryPlaylist;
 import com.kuit.agarang.domain.playlist.model.entity.Playlist;
 import com.kuit.agarang.domain.playlist.repository.MemoryPlaylistRepository;
-import com.kuit.agarang.domain.playlist.repository.PlaylistRepository;
 import com.kuit.agarang.domain.playlist.util.MusicSaveUtil;
 import com.kuit.agarang.global.common.exception.exception.BusinessException;
 import com.kuit.agarang.global.common.exception.exception.OpenAPIException;
@@ -29,14 +32,13 @@ import com.kuit.agarang.global.s3.model.dto.S3File;
 import com.kuit.agarang.global.s3.utils.S3FileUtil;
 import com.kuit.agarang.global.s3.utils.S3Util;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Slf4j
 @Service
@@ -46,26 +48,21 @@ public class AIService {
   private final GPTUtil gptUtil;
   private final GPTPromptUtil promptUtil;
   private final GPTChatService gptChatService;
+  private final MusicGenService musicGenService;
   private final TypecastService typecastService;
 
   private final S3Util s3Util;
   private final S3FileUtil s3FileUtil;
+  private final MusicSaveUtil musicSaveUtil;
 
   private final RedisService redisService;
   private final ObjectMapper objectMapper;
 
   private final MemoryRepository memoryRepository;
   private final MemberRepository memberRepository;
-
-
-  private final MusicGenService musicGenService;
-  private final HashTagRepository hashTagRepository;
-
-  private final MusicSaveUtil musicSaveUtil;
   private final MemoryPlaylistRepository memoryPlaylistRepository;
-  private final PlaylistRepository playlistRepository;
 
-  public QuestionResponse getFirstQuestion(MultipartFile image) throws Exception {
+  public QuestionResponse getFirstQuestion(MultipartFile image) {
     S3File convertedImage = s3FileUtil.convert(image);
 
     // image -> gpt -> 노래제목, 해시태그 생성
@@ -79,7 +76,7 @@ public class AIService {
     String question = gptUtil.getGPTAnswer(questionChat);
 
     // 질문1 -> tts -> 오디오 변환
-    String questionAudioUrl = getAudioUrl(question);
+    String typecastAudioId = typecastService.getAudioDownloadUrl(question);
 
     // 대화기록 저장 및 임시저장이 필요한 데이터 저장
     String redisKey = questionChat.getGptResponse().getId();
@@ -91,16 +88,14 @@ public class AIService {
         .historyMessages(historyMessage)
         .build());
 
-    logChat(historyMessage);
     return new QuestionResponse(QuestionResult.builder()
       .id(redisKey)
       .text(question)
-      .audioUrl(questionAudioUrl)
+      .audioUrl(getAudioUrl(typecastAudioId))
       .build());
   }
 
-  private @Nullable String getAudioUrl(String question) {
-    String typecastAudioId = typecastService.getAudioDownloadUrl(question);
+  private @Nullable String getAudioUrl(String typecastAudioId) {
     String questionAudioUrl = null;
     if (checkEntityExistence(typecastAudioId)) {
       questionAudioUrl = redisService.get(typecastAudioId, String.class)
@@ -117,15 +112,15 @@ public class AIService {
     GPTChat chat = gptChatService.chatWithHistory(chatHistory.getHistoryMessages(), answer.getText(), 0L);
     String question = gptUtil.getGPTAnswer(chat);
 
-    String questionAudioUrl = getAudioUrl(question);
+    String typecastAudioId = typecastService.getAudioDownloadUrl(question);
 
-    logChat(gptUtil.createHistoryMessage(chat));
+    gptUtil.addHistoryMessage(chatHistory, gptUtil.getResponseMessage(chat));
     redisService.save(answer.getId(), chatHistory);
 
     return new QuestionResponse(QuestionResult.builder()
       .id(answer.getId())
       .text(question)
-      .audioUrl(questionAudioUrl)
+      .audioUrl(getAudioUrl(typecastAudioId))
       .build());
   }
 
@@ -133,9 +128,9 @@ public class AIService {
     GPTChatHistory chatHistory = redisService.get(answer.getId(), GPTChatHistory.class)
       .orElseThrow(() -> new OpenAPIException(BaseResponseStatus.NOT_FOUND_HISTORY_CHAT));
 
-    chatHistory.getHistoryMessages().add(gptUtil.createTextMessage(answer.getText()));
-    logChat(chatHistory.getHistoryMessages());
+    gptUtil.addHistoryMessage(chatHistory, gptUtil.createTextMessage(answer.getText()));
     redisService.save(answer.getId(), chatHistory);
+    logChat(chatHistory.getHistoryMessages());
   }
 
   @Async
@@ -149,7 +144,7 @@ public class AIService {
     GPTChat chat = gptChatService.chatWithHistory(chatHistory.getHistoryMessages(), prompt, 0L);
 
     chatHistory.setMemoryText(gptUtil.getGPTAnswer(chat));
-    logChat(gptUtil.createHistoryMessage(chat));
+    gptUtil.addHistoryMessage(chatHistory, gptUtil.getResponseMessage(chat));
     redisService.save(gptChatHistoryId, chatHistory);
   }
 
@@ -177,14 +172,12 @@ public class AIService {
     String prompt = promptUtil.createMusicGenPrompt(chatHistory.getImageDescription(), chatHistory.getMusicInfo());
     GPTChat chat = gptChatService.chat(GPTSystemRole.MUSIC_PROMPT_ENGINEER, prompt, 1L, true);
     String musicGenPrompt = gptUtil.parseJson(chat, "prompt");
-    logChat(gptUtil.createHistoryMessage(chat));
-    log.info("musicGenPrompt {}", musicGenPrompt);
+    log.info("musicGenPrompt : {}", musicGenPrompt);
 
     prompt = promptUtil.createMusicTitlePrompt(musicGenPrompt, chatHistory.getMusicInfo());
     chat = gptChatService.chat(GPTSystemRole.MUSIC_TITLE_WRITER, prompt, 1L, true);
     String musicTitle = gptUtil.parseJson(chat, "music_name");
-    logChat(gptUtil.createHistoryMessage(chat));
-    log.info(musicTitle);
+    log.info("musicTitle : {}", musicTitle);
 
     String musicGenId = musicGenService.getMusic(musicGenPrompt);
     S3File image = s3Util.upload(chatHistory.getImage());
@@ -223,12 +216,14 @@ public class AIService {
       .orElseThrow(() -> new BusinessException(BaseResponseStatus.NOT_FOUND_MEMBER));
   }
 
-  // TODO : redis 트리거 전환
   public boolean checkEntityExistence(String key) {
+    if (redisService.existsByKey(key)) {
+      return true;
+    }
     try {
       for (int i = 1; i < 3; i++) {
         log.info("{}차 대기", i);
-        Thread.sleep(1500);
+        Thread.sleep(1000);
 
         if (redisService.existsByKey(key)) {
           return true;
